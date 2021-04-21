22e161b5521cbe29c4e3a1a2da06c2e3e2ea683b
Bishree <ahmedbishree@gmail.com@users.noreply.github.com>
2021-04-21 03:54:25 +0300

    _first_commit_2

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+﻿fn main() {
+    enum spreadSheetcell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+
+    }
+    let row = vec![
+        spreadSheetcell::Int(77),
+        spreadSheetcell::Text(String::from("blue")),
+        spreadSheetcell::Float(10.20),
+    ];
+
+}
